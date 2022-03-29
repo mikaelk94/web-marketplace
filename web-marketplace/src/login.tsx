a23837e58bdacb7ceaacc9eb28b89a4035d9912b
Petri Pennanen <t9pepe01@students.oamk.fi>
2022-03-29 21:00:34 +0300

    Lisätty kirjautumissivu

--- web-marketplace/src/login.tsx
+++ web-marketplace/src/login.tsx
@@ -1,17 +1,54 @@
 import React from 'react'
 import './App.css'
 import Navbar from './components/navbar/navbar'
-import Search from './components/search/Search'
+import axios from 'axios'
+import './login.css'
+
+const session_url = 'https://verkkokauppa-api.herokuapp.com/login'
+
+
+const loginfunc = async () => {
+    let user = (document.getElementById('username') as HTMLInputElement).value
+    let pass = (document.getElementById('password') as HTMLInputElement).value
+    axios.post(session_url, {}, {
+        auth: {
+            username: user,
+            password: pass
+        }
+    }).then(function (response) {
+        console.log('Authenticated');
+        console.log(response.data);
+        console.log(user, pass);
+    }).catch(function (error) {
+        console.log('Error on Authentication');
+    });
+}
 
 function Login() {
     return (
         <div className='etusivu'>
             <Navbar />
 
-            <div>
-                <h1>Welcome to login!</h1>
+            <div className='login'>
             </div>
-        </div>
+            <div>
+                <form className='loginForm'>
+                    <div className='form-inner'>
+                        <h2>Kirjaudu sisään</h2>
+                        <div className='form-group'>
+                            <label htmlFor='name'>Käyttäjänimi</label>
+                            <input type='text' name='name' id='username' />
+                        </div>
+                        <div className='form-group'>
+                            <label htmlFor='password'>Salasana</label>
+                            <input type="password" name="password" id="password" />
+                        </div>
+                        <button className='btn' onClick={loginfunc}>Kirjaudu sisään</button>
+                        <button className='btn'>Rekisteröidy</button>
+                    </div>
+                </form>
+            </div >
+        </div >
     )
 }
 
