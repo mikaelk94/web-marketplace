b95f879e1083ed7ac4c73cf1515efa6638284b0e
Mikael Kanniainen <kanniainen.mikael@gmail.com>
2022-04-13 18:03:23 +0300

    omat postaukset
    Postausten poisto lisätty myposts.tsx filuun.

--- web-marketplace/src/components/productCard/ProductCard.tsx
+++ web-marketplace/src/components/productCard/ProductCard.tsx
@@ -5,15 +5,21 @@ interface Props {
   location?: string
   image?: string
   price?: string
+  btnDelete?: any
+  btnEdit?: any
 }
 
 const ProductCard = (props: Props) => {
-  const { title, location, image, price } = props
+  const { title, location, image, price, btnDelete, btnEdit } = props
   const croppedImage = image?.replace('upload/', 'upload/c_fit,w_330,h_200/')
 
   return (
     <div className='product-card'>
-      <div className='product-title'>{title}</div>
+      <div className='product-title'>
+        {title}
+        {btnEdit}
+        {btnDelete}
+      </div>
       <div className='product-location'>{location}</div>
       <div className='image-div'>
         <img src={croppedImage} />
