bf22b13740b254b93b35cab197e69dd6941aa06f
sholappa <sa.holappa@gmail.com>
2022-04-14 23:16:50 +0300

    login ja register bootstrapilla

--- web-marketplace/src/components/register/register.tsx
+++ web-marketplace/src/components/register/register.tsx
@@ -4,7 +4,7 @@ import { useState, useRef, useContext } from 'react'
 import { UserContext } from '../../context/UserContext'
 import Nav_bar from '../navbar/navbar'
 import axios from 'axios'
-
+import { Form, Button, } from 'react-bootstrap'
 const session_url = 'https://verkkokauppa-api.herokuapp.com/users'
 
 function checkpassword() {
@@ -49,19 +49,17 @@ function Register() {
   }
 
   return (
+    <Form>
+     <Nav_bar />
     <div className='etusivu'>
-      <Nav_bar />
-      <div className='login'></div>
-      <div>
-        <form className='registerForm'>
-          <div className='form-inner'>
+      <div className='register'>
             <h2>Rekisteröidy</h2>
             <div className='errorLabel'>
               <label htmlFor='error' id='error'></label>
             </div>
-            <div className='form-group'>
-              <label htmlFor='name'>Käyttäjänimi</label>
-              <input
+            <Form.Group className='form-group1'>
+              <Form.Label htmlFor='name'>Käyttäjänimi</Form.Label>
+              <Form.Control
                 ref={inputRef}
                 type='text'
                 name='name'
@@ -69,87 +67,99 @@ function Register() {
                 value={username}
                 onChange={(e) => setUsername(e.target.value)}
               />
-            </div>
-            <div className='form-group'>
-              <label htmlFor='password'>Salasana</label>
-              <input
+            </Form.Group>
+            <Form.Group className='form-group2'>
+              <Form.Label htmlFor='password'>Salasana</Form.Label>
+              <Form.Control
                 type='password'
                 name='password'
                 id='password'
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
               />
-            </div>
-            <div className='form-group'>
-              <label htmlFor='passwordcheck'>Varmista salasana</label>
-              <input
+            </Form.Group>
+            <Form.Group className='form-group'>
+              <Form.Label htmlFor='passwordcheck'>Varmista salasana</Form.Label>
+              <Form.Control
                 type='password'
                 name='passwordcheck'
                 id='passwordcheck'
                 value={passwordcheck}
                 onChange={(e) => setPasswordcheck(e.target.value)}
               />
-            </div>
-            <div className='form-group'>
-              <label htmlFor='firstname'>Etunimi</label>
-              <input
+            </Form.Group>
+            <Form.Group className='form-group'>
+              <Form.Label htmlFor='firstname'>Etunimi</Form.Label>
+              <Form.Control
                 type='text'
                 name='firstname'
                 id='firstname'
                 value={firstname}
                 onChange={(e) => setFirstname(e.target.value)}
               />
-            </div>
-            <div className='form-group'>
-              <label htmlFor='lastname'>Sukunimi</label>
-              <input
+            </Form.Group>
+            <Form.Group className='form-group'>
+              <Form.Label htmlFor='lastname'>Sukunimi</Form.Label>
+              <Form.Control
                 type='text'
                 name='lastname'
                 id='lastname'
                 value={lastname}
                 onChange={(e) => setLastname(e.target.value)}
               />
-            </div>
-            <div className='form-group'>
-              <label htmlFor='email'>Sähköposti</label>
-              <input
+            </Form.Group>
+            <Form.Group className='form-group'>
+              <Form.Label htmlFor='email'>Sähköposti</Form.Label>
+              <Form.Control
                 type='email'
                 name='email'
                 id='email'
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
               />
-            </div>
-            <div className='form-group'>
-              <label htmlFor='PhoneNum'>Puhelinumero</label>
-              <input
+            </Form.Group>
+            <Form.Group className='form-group'>
+              <Form.Label htmlFor='PhoneNum'>Puhelinumero</Form.Label>
+              <Form.Control
                 type='text'
                 name='phonenum'
                 id='phonenum'
                 value={phonenumber}
                 onChange={(e) => setPhonenumber(e.target.value)}
               />
-            </div>
-            <div className='form-group'>
-              <label htmlFor='dateOfBirth'>Syntymäaika</label>
-              <input
+            </Form.Group>
+            <Form.Group className='form-group'>
+              <Form.Label htmlFor='dateOfBirth'>Syntymäaika</Form.Label>
+              <Form.Control
                 type='text'
                 name='dateofbith'
                 id='dateofbirth'
                 value={dateofbirth}
                 onChange={(e) => setDateofbirth(e.target.value)}
               />
-            </div>
-            <button className='btn' onClick={sendJson}>
+            </Form.Group>
+            
+            <Button
+            onClick={sendJson}
+            variant='success'
+            className='mt-3 mt-4'
+            size='lg'
+            active={true}
+            type='submit'           
+            >
               Rekisteröidy
-            </button>
-            <div className='link'>
-              <a href='login'>Kirjaudu sisään</a>
-            </div>
-          </div>
-        </form>
+            </Button>
+            <Button
+            variant='success'
+            className='mt-3 mt-4'
+            size='lg'
+            active={true}
+            type='submit'
+            href="login">Takaisin
+            </Button>
       </div>
     </div>
+  </Form>
   )
 }
 
